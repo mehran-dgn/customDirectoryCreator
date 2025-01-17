d03e350f80ac9d43f6fc31f4880d54dc0cec3ab4
mehran-dgn <mehrandehghanan@yahoo.com>
2025-01-17 10:29:15 +0330

    the base functionality of the program implemented , user can create folder in a specific directory and can paste files from clipboard into that created directory , still needs work for better optimization and user experience and also should work on the interface style

--- app.py
+++ app.py
@@ -1,20 +1,105 @@
-from functions import createFolder
-from tkinter import filedialog
-import tkinter as tk 
+import tkinter as tk  
+from tkinter import filedialog, messagebox  
+import os  
+import shutil  
+import win32clipboard  
+
+rootDirectory = ""  
+createdFolder = ""  
+
+def askUserToChooseRootDir():  
+    global rootDirectory  
+    rootDirectory = filedialog.askdirectory()  
+    print(f"Selected root directory: {rootDirectory}")  
+
+def createFolder():  
+    global createdFolder  
+    baseDir = rootDirectory  
+    createdFolder = fileNameInput.get()  
+
+    if createdFolder:  
+        finalDirectory = os.path.join(baseDir, createdFolder)  
+        try:  
+            os.mkdir(finalDirectory)  
+            print(f"Directory '{finalDirectory}' created successfully.")    
+            createdFolder = finalDirectory  
+            # fileNameInput.delete(0, tk.END)
+        except FileExistsError:  
+            error_message = "مشکلی پیش آمده است، بررسی کنید پوشه ای با این نام قبلاً ساخته نشده باشد"  
+            messagebox.showerror("Error", error_message) 
+        except Exception as e:  
+            messagebox.showerror("Error", str(e))    
+
+def pasteFromClipboard():  
+    global rootDirectory, createdFolder  
+
+    if not createdFolder:  
+        messagebox.showwarning("Warning", "Please create a folder first.")  
+        return  
+
+    try:  
+        win32clipboard.OpenClipboard()  
+        if win32clipboard.IsClipboardFormatAvailable(win32clipboard.CF_HDROP):  
+            file_list = win32clipboard.GetClipboardData(win32clipboard.CF_HDROP)
+            print(f"Files in clipboard: {file_list}")  
+            for file_path in file_list:  
+                print(f"Processing file: {file_path}")  
+                if os.path.isfile(file_path):  
+                    if file_path.lower().endswith(('.jpg', '.jpeg', '.png')): 
+                        try:  
+                            shutil.copy(file_path, createdFolder)  
+                            print(f"Copied {file_path} to {createdFolder}.")  
+                            
+                        except Exception as e:  
+                            messagebox.showerror("Error", f"Failed to copy {file_path}: {str(e)}")  
+                    else:  
+                        messagebox.showwarning("Warning", f"{file_path} is not a valid image file.")  
+                else:  
+                    messagebox.showwarning("Warning", f"{file_path} is not a valid file path in clipboard.")  
+
+        else:  
+            messagebox.showwarning("Warning", "The clipboard does not contain file paths.")  
+
+    except Exception as e:  
+        messagebox.showerror("Error", f"Failed to read the clipboard: {str(e)}")  
+    finally:  
+        win32clipboard.CloseClipboard()  
+        rootDirectory = ""  
+        createdFolder = ""  
+
+root = tk.Tk()  
+root.geometry("600x400")  
+root.title("نرم افزار مدیریت و ساخت پوشه ها")  
+
+mainFrame = tk.Frame(root)  
+mainFrame.grid(row=0, column=0, sticky="nsew")  
 
 
-root = tk.Tk()
-root.title("نرم افزار مدیریت مدارک")
-root.geometry("400x300")
+lbl1 = tk.Label(mainFrame, text=":مسیر ذخیره سازی را انتخاب کنید")  
+lbl1.grid(row=0, column=1, sticky="ew")  
 
-directory_label = tk.Label(root,text=":مسیر انتخاب شده")
-directory_label.pack(side="right")
+chooseDirBtn = tk.Button(mainFrame, text="انتخاب", command=askUserToChooseRootDir, width=5)  
+chooseDirBtn.grid(row=0, column=0, sticky="ew")  
+
+fileNameLabel = tk.Label(mainFrame, text=":نام فایل را وارد کنید")  
+fileNameLabel.grid(row=1, column=1, sticky="ew")  
+
+fileNameInput = tk.Entry(mainFrame)  
+fileNameInput.grid(row=1, column=0, sticky="ew")  
+
+createFolderBtn = tk.Button(mainFrame, text="ایجاد پوشه", command=createFolder)  
+createFolderBtn.grid(row=2, column=0, sticky="ew")  
+
+pasteButton = tk.Button(mainFrame, text="چسباندن از کلیپ بورد", command=pasteFromClipboard)  
+pasteButton.grid(row=3, column=0, sticky="ew")  
 
 
-folderNameInput = tk.Text(root , height = 5 , width=20)
-folderNameInput.pack()
+root.grid_rowconfigure(0, weight=1)  
+root.grid_columnconfigure(0, weight=1)  
+mainFrame.grid_rowconfigure(0, weight=1)  
+mainFrame.grid_columnconfigure(0, weight=1)  
+mainFrame.grid_columnconfigure(1, weight=1)  
 
-printButton = tk.Button(root,text="print" , command=lambda: createFolder(createFolder.get("1.0","end-1c")))
-printButton.pack()
+mainFrame.pack()  
 
 root.mainloop()
--- functions.py
+++
@@ -1,9 +0,0 @@
-import os 
-from tkinter import filedialog
-
-def chooseTheRootDirectory():
-    selected_directory = filedialog.askdirectory()
-    
-
-def createFolder(folderName):
-    pass 

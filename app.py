da42e2b5c28db4d131d892d3649210d4d9043827
mehran-dgn <mehrandehghanan@yahoo.com>
2025-01-18 01:48:59 +0330

    the last chosen base directory saved in the txt file to create folders the last selected directory and not force the user each time to select the base directory, error handlings fixed , the main logic of the app improved , the 1st stable version finished , but still  there are ui tasks

--- app.py
+++ app.py
@@ -4,39 +4,80 @@ import os
 import shutil  
 import win32clipboard  
 
-rootDirectory = ""  
-createdFolder = ""  
-whoIsThisForVal = ""
 
-def askUserToChooseRootDir():  
-    global rootDirectory  
-    rootDirectory = filedialog.askdirectory()  
-    print(f"Selected root directory: {rootDirectory}")  
+rootDirectory = ""
+try:
+    with open("usedDirectories.txt", "r") as f:
+        saved_directory = f.readline().strip()
+        if saved_directory:
+            rootDirectory = saved_directory
+            print(f"Loaded base directory: {rootDirectory}")
+except FileNotFoundError:
+    print("No saved directory found")
+
+
+createdFolder = ""  
+optionalDesc = ""
+canSubmit = False   
+
+
+
+def checkBaseDirectory():
+    global rootDirectory
+    try:
+        with open("usedDirectories.txt", "r") as f:
+            first_directory = f.readline().strip()
+            if first_directory:
+                rootDirectory = first_directory
+                return True
+    except FileNotFoundError:
+        messagebox.showwarning("هشدار", "لطفا یک مسیر پایه را انتخاب کنید")
+        return False
+    return False
+
+def askUserToChooseRootDir():
+    global rootDirectory
+    selected_directory = filedialog.askdirectory()
+    
+    if selected_directory:
+        rootDirectory = selected_directory
+        with open("usedDirectories.txt", "w") as f:
+            f.write(f"{rootDirectory}")
+        print(f"Selected root directory: {rootDirectory}")
 
 def createFolder():  
-    global createdFolder  
+    global createdFolder 
+    global canSubmit 
+    
+    if not rootDirectory:
+        messagebox.showwarning("هشدار", "لطفا ابتدا مسیر پایه را انتخاب کنید")
+        return
+        
     baseDir = rootDirectory  
     createdFolder = fileNameInput.get()  
+    canSubmit = True 
 
-    if createdFolder:  
+    if createdFolder and canSubmit == True:  
         finalDirectory = os.path.join(baseDir, createdFolder)  
         try:  
             os.mkdir(finalDirectory)  
             print(f"Directory '{finalDirectory}' created successfully.")    
             createdFolder = finalDirectory  
-            # fileNameInput.delete(0, tk.END)
         except FileExistsError:  
             error_message = "مشکلی پیش آمده است، بررسی کنید پوشه ای با این نام قبلاً ساخته نشده باشد"  
             messagebox.showerror("Error", error_message) 
         except Exception as e:  
-            messagebox.showerror("Error", str(e))    
+            messagebox.showerror("Error", str(e))
+ 
 
 def pasteFromClipboard():  
     global rootDirectory, createdFolder  
-
-    if not createdFolder:  
-        messagebox.showwarning("Warning", "Please create a folder first.")  
-        return  
+    global canSubmit
+    global optionalDesc
+    isSubmitted = False
+    if not rootDirectory or not createdFolder:
+        messagebox.showwarning("هشدار", "لطفا ابتدا مسیر و پوشه را ایجاد کنید")
+        return
 
     try:  
         win32clipboard.OpenClipboard()  
@@ -50,33 +91,46 @@ def pasteFromClipboard():
                         try:  
                             shutil.copy(file_path, createdFolder)  
                             print(f"Copied {file_path} to {createdFolder}.")  
+                            isSubmitted = True 
+                            canSubmit = False 
+
                             
                         except Exception as e:  
-                            messagebox.showerror("Error", f"Failed to copy {file_path}: {str(e)}")  
+                            messagebox.showerror("خطا", f"Failed to copy {file_path}: {str(e)}")  
                     else:  
-                        messagebox.showwarning("Warning", f"{file_path} is not a valid image file.")  
+                        messagebox.showwarning("هشدار", f"{file_path} is not a valid image file.")  
                 else:  
-                    messagebox.showwarning("Warning", f"{file_path} is not a valid file path in clipboard.")  
+                    messagebox.showwarning("هشدار", f"{file_path} is not a valid file path in clipboard.")  
 
         else:  
-            messagebox.showwarning("Warning", "The clipboard does not contain file paths.")  
+            messagebox.showwarning("هشدار", "The clipboard does not contain file paths.")  
 
     except Exception as e:  
-        messagebox.showerror("Error", f"Failed to read the clipboard: {str(e)}")  
+        messagebox.showerror("خطا", f"Failed to read the clipboard: {str(e)}")  
     finally:  
         win32clipboard.CloseClipboard()  
-        createdFolder = ""  
+        createdFolder = "" 
+        optionalDesc = "" 
+        if isSubmitted:
+            fileNameInput.delete(0, tk.END)
+            submitOptionalDesc.delete(0, tk.END)
+            messagebox.showinfo("پیام نرم افزار","انتقال فایل ها با موفقیت انجام شد")
+
 
 
 def submitPerson():
-    global whoIsThisForVal
-    whoIsThisForVal = str(submitWhoIsThisFor.get())
-    print("shittt")
-    directoryToSaveNote =  os.path.join(rootDirectory, createdFolder)  
+    global optionalDesc
+    global createdFolder
+    if not rootDirectory or not createdFolder:
+        messagebox.showerror("خطا","لطفا ابتدا مسیر و پوشه را ایجاد کنید")
+        return
+        
+    optionalDesc = str(submitOptionalDesc.get())
+    directoryToSaveNote = os.path.join(rootDirectory, createdFolder)
     file = open(f"{directoryToSaveNote}/details.txt","w")
-    print(whoIsThisForVal)
-    file.write(f"شماره به نام : {whoIsThisForVal}")
+    file.write(f"{optionalDesc}")
     file.close()
+
 
 root = tk.Tk()  
 root.geometry("340x150")  
@@ -84,6 +138,8 @@ root.title("نرم افزار مدیریت و ساخت پوشه ها")
 
 mainFrame = tk.Frame(root)  
 mainFrame.grid(row=0, column=0, sticky="nsew")  
+
+
 
 
 lbl1 = tk.Label(mainFrame, text=":مسیر ذخیره سازی را انتخاب کنید")  
@@ -101,11 +157,11 @@ fileNameInput.grid(row=1, column=0, sticky="ew")
 createFolderBtn = tk.Button(mainFrame, text="ایجاد پوشه", command=createFolder)  
 createFolderBtn.grid(row=2, column=0, sticky="ew")  
 
-whoIsThisFor = tk.Label(mainFrame, text=":به نام")
-whoIsThisFor.grid(row=3, column=2, sticky="ew")
+optionalDesc = tk.Label(mainFrame, text=":توضیحات")
+optionalDesc.grid(row=3, column=2, sticky="ew")
 
-submitWhoIsThisFor = tk.Entry(mainFrame)
-submitWhoIsThisFor.grid(row=3, column=1, sticky="ew")
+submitOptionalDesc = tk.Entry(mainFrame)
+submitOptionalDesc.grid(row=3, column=1, sticky="ew")
 
 pasteButton = tk.Button(mainFrame, text="ثبت", command=submitPerson)  
 pasteButton.grid(row=3, column=0, sticky="ew")  
